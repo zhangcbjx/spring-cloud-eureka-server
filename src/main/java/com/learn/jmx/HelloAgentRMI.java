3193d3aafad04e30e767c93291645d0f73012d42
zhangcb <zhangcb@LAPTOP-FC9BT7S5>
2019-04-28 10:43:46 +0800

    ====

--- src/main/java/com/learn/jmx/HelloAgentRMI.java
+++ src/main/java/com/learn/jmx/HelloAgentRMI.java
@@ -34,7 +34,6 @@ public class HelloAgentRMI {
 		// 这句话非常重要，不能缺少！注册一个端口，绑定url后，客户端就可以使用rmi通过url方式来连接JMXConnectorServer
 		Registry registry = LocateRegistry.createRegistry(1099);
 		
-		
 		// 构造JMXServiceURL
 		JMXServiceURL jmxServiceURL = new JMXServiceURL("service:jmx:rmi:///jndi/rmi://localhost:1099/jmxrmi");
 		// 创建JMXConnectorServer
